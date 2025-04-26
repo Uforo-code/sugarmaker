235838bf4c719cc149eb1bdc1d31460053117be9
Uforo-code <essienuforo081@gmail.com>
2025-04-26 12:27:15 +0200

    Update run-testnet-pool.bat

--- mining-script/bat/run-testnet-pool.bat
+++ mining-script/bat/run-testnet-pool.bat
@@ -1,1 +1,1 @@
-sugarmaker.exe -a YespowerAdvc -o stratum+tcp://testnet.novagrid.online:3100 -u aQcrcxKbyfaGs3ZoYXdhjtDLVSNLp7j4xQ -t1
+sugarmaker.exe -a YespowerAdvc -o stratum+tcp://testnet.novagrid.online:3100 -u aRD2yrp2ZUiMgndTNw8MB59tmiMwzSmv2f-t1
